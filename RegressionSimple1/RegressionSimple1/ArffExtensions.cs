aa0535f3e30d1689eee8659319e1705b82f1da3c
Roman Schacherl <roman.schacherl@softaware.at>
2019-11-14 14:59:41 +0100

    Read arff file

---
+++ RegressionSimple1/RegressionSimple1/ArffExtensions.cs
@@ -0,0 +1,31 @@
+﻿using ArffTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegressionSimple1
+{
+    public static class ArffExtensions
+    {
+        public static T MapTo<T>(this object[] instance, ArffHeader arffHeader)
+        {
+            var type = typeof(T);
+            var properties = type.GetProperties();
+            var data = Activator.CreateInstance<T>();
+
+            for (int column = 0; column < instance.Length; column++)
+            {
+                var attribute = arffHeader.Attributes[column];
+                var property = properties.FirstOrDefault(p => p.Name.Equals(attribute.Name, StringComparison.CurrentCultureIgnoreCase));
+
+                if (property != null)
+                {
+                    property.SetValue(data, instance[column]);
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ RegressionSimple1/RegressionSimple1/Program.cs
@@ -0,0 +1,28 @@
+﻿using ArffTools;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+
+namespace RegressionSimple1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataFolder = @"D:\softaware\samples-ml\data";
+
+            var wineReviews = new List<WineReview>();
+
+            using (ArffReader arffReader = new ArffReader($"{dataFolder}\\openml-wine_reviews.arff"))
+            {
+                var header = arffReader.ReadHeader();
+                object[][] instances = arffReader.ReadAllInstances();
+
+                for (int row = 0; row < instances.GetLength(0); row++)
+                {
+                    wineReviews.Add(instances[row].MapTo<WineReview>(header));
+                }
+            }
+        }
+    }
+}
