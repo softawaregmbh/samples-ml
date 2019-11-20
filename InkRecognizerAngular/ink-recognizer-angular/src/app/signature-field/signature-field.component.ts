7858042f67cfa30e01fc63eaf0e6984b43134e67
Roman Schacherl <roman.schacherl@softaware.at>
2019-11-20 14:23:25 +0100

    Add new angular project for inkrecognizer

---
+++ InkRecognizerAngular/ink-recognizer-angular/src/app/signature-field/signature-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signature-field',
+  templateUrl: './signature-field.component.html',
+  styleUrls: ['./signature-field.component.scss']
+})
+export class SignatureFieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
